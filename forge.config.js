4a0ad4ea2bfe4db4bb122677bfc057659ec7b763
chengeng <chengeng@bloggeng.com>
2022-11-24 12:09:13 +0800

    fix(模块): 修改打包配置

--- forge.config.js
+++ forge.config.js
@@ -1,22 +1,45 @@
 module.exports = {
-  packagerConfig: {},
-  rebuildConfig: {},
-  makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
+    publishers: [
+        {
+            "name": "@electron-forge/publisher-github",
+            "config": {
+                "repository": {
+                    "owner": "leochen-g",
+                    "name": "wechat-assistant-eletron"
+                },
+                "prerelease": false,
+                "draft": true
+            }
+        }
+    ],
+    packagerConfig: {
+        "appVersion": "0.0.1",
+        "name": "智能微秘书客户端",
+        "appCopyright": "Leo_chen",
+        "icon": "./assets/img/appIcon/favicon",
+        "win32metadata": {
+            "ProductName": "智能微秘书 Windows",
+            "CompanyName": "aibotk.com",
+            "FileDescription": "智能微秘书客户端"
+        }
     },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
-  ],
+    rebuildConfig: {},
+    makers: [
+        {
+            name: '@electron-forge/maker-squirrel',
+            config: {},
+        },
+        {
+            name: '@electron-forge/maker-zip',
+            platforms: ['darwin'],
+        },
+        {
+            name: '@electron-forge/maker-deb',
+            config: {},
+        },
+        {
+            name: '@electron-forge/maker-rpm',
+            config: {},
+        },
+    ],
 };
